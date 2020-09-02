7b3df42dbdb963901eea5c1e43e53c71be175ba4
Nazareno <Aeliner123@gmail.com>
2020-09-02 18:38:02 -0300

    Added a simple loading component, api calls are working and it's currently showing weather information and forecast for a fixed city

--- src/App.js
+++ src/App.js
@@ -1,15 +1,72 @@
-import React from 'react';
-import WeatherComp from './components/weather';
-import './App.css';
+import React, { Component } from "react";
+import Moment from "moment";
+import WeatherComp from "./components/weather";
+import Loading from "./components/loading";
+import "./App.css";
 
+const API = {
+  base: "",
+  key: "3d3c2fd8419ae71eb8bbca7c783dae82",
+};
 
+function renderChildComponent() {
+  const { data } = this.state;
+  const { forecast } = this.state;
+  if (data && forecast) {
+    return <WeatherComp data={this.state.data} forecast={this.state.forecast}></WeatherComp>;
+  }
+return <Loading></Loading>;
+}
 
-function App() {
+//https://api.openweathermap.org/data/2.5/find?q=florida&appid=3d3c2fd8419ae71eb8bbca7c783dae82&units=metric
+class WeatherApp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: undefined,
+      forecast: undefined,
+    };
+  }
+
+  componentDidMount() {
+    this.getWeatherInfo();
+  }
+
+  getWeatherInfo = async () => {
+    return await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=paysandu,&appid=" +
+      API.key +
+      "&units=metric"
+    )
+      .then((response) => response.json())
+      .then((data) => this.getOneCallApi(data))
+      .catch((error) => console.log(error));
+  };
+
+  getOneCallApi = async (data) => {
+    this.setState({ data: data });
+    return await fetch(
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&exclude=daily,minutely,current&appid=" + API.key + "&units=metric").then((response) => response.json())
+      .then((data) => this.getHourlyForecast(data))
+      .catch((error) => console.log(error));
+  };
+
+   getHourlyForecast = (data) => {
+  let newData = data.hourly.slice(1, 8);
+  for (let i = 0; i < newData.length; i++) {
+    let t = newData[i].dt;
+    newData[i].dt = Moment.unix(t).format('LT');
+  }
+  this.setState({ forecast: newData });
+};
+
+render() {
   return (
     <div className="App">
-      <WeatherComp></WeatherComp>
+      {renderChildComponent.call(this)}
     </div>
   );
 }
+}
 
-export default App;
+export default WeatherApp;
--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 
-const WeatherComp = () => {
+const WeatherComp = (props) => {
   return (
     <div className="container">
-      <h1>Weather App</h1>
+      <h1>Current weather for {props.data.name},{props.data.sys.country}</h1>
+      <div className="current-weather-info">
+          <div className="temperature">
+            <h3>{props.data.main.temp}°C</h3> 
+            <img src={require("../icons/"+ props.data.weather[0].id+".svg")}   style={{filter: "invert(1)"}} alt=""/>
+            </div>
+          <div className="min-max">
+              {props.data.main.temp_min}
+              {props.data.main.temp_max}
+          </div>
+      </div>
+      <div className="forecast-info">
+          <ul className="weather-list">
+              {props.forecast.map((hour,i) =>(
+                <li key={"hour-"+i}className="weather-card"><span className="UpperText">{hour.dt}</span><img src={require("../icons/"+ hour.weather[0].id+".svg")} style={{filter: "invert(1)"}} alt=""/><span className="BottomText">{Math.round(hour.temp)}°C</span></li>
+              ))}
+          </ul>
+      </div>
     </div>
   );
 };
